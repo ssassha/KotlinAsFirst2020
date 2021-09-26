780fcd7071d14709ab982e8f786464c7cb3d6829
IrReiku <irreiku@mail.ru>
2021-09-26 12:05:25 +0300

    > Исправлено время выполнения задач fib и fibSequenceDigit в 3-ем уроке

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -84,11 +84,17 @@ fun digitNumber(n: Int): Int {
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int =
-    when (n) {
-        1, 2 -> 1
-        else -> fib(n - 1) + fib(n - 2)
+fun fib(n: Int): Int {
+    var a = 0
+    var b = 1
+    var p: Int
+    for (j in 2..n) {
+        p = b
+        b += a
+        a = p
     }
+    return b
+}
 
 
 /**
