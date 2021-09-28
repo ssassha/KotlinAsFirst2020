e7122dfcc9ff7f839b120e07cd2f21e8fdf22e28
al_ga <al_ganin@mail.ru>
2021-09-29 00:51:08 +0300

    Исправлены новые замечания к первому и второму урокам + сделана часть третьего урока

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -2,7 +2,7 @@
 
 package lesson3.task1
 
-import kotlin.math.sqrt
+import kotlin.math.*
 
 // Урок 3: циклы
 // Максимальное количество баллов = 9
@@ -72,7 +72,11 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int =
+    when {
+        n / 10 == 0 -> 1
+        else -> digitNumber(n / 10) + 1
+    }
 
 /**
  * Простая (2 балла)
@@ -80,21 +84,39 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    var a = 1
+    var b = 1
+    var c: Int
+    var res = 2
+    while (res < n) {
+        res += 1
+        c = b
+        b += a
+        a = c
+    }
+    return b
+}
 
 /**
  * Простая (2 балла)
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    for (i in 2..sqrt(n.toDouble()).toInt()) if (n % i == 0) return i
+    return n
+}
 
 /**
  * Простая (2 балла)
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    for (i in n / 2 downTo 2) if (n % i == 0) return i
+    return 1
+}
 
 /**
  * Простая (2 балла)
@@ -112,7 +134,13 @@ fun maxDivisor(n: Int): Int = TODO()
  * Написать функцию, которая находит, сколько шагов требуется для
  * этого для какого-либо начального X > 0.
  */
-fun collatzSteps(x: Int): Int = TODO()
+fun collatzSteps(x: Int): Int =
+    when {
+        x == 1 -> 0
+        x == 2 -> 1
+        x == 4 -> 2
+        else -> if (x % 2 == 0) collatzSteps(x / 2) + 1 else collatzSteps(x * 3 + 1) + 1
+    }
 
 /**
  * Средняя (3 балла)
@@ -120,7 +148,17 @@ fun collatzSteps(x: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int {
+    var a = maxOf(m, n)
+    var b = minOf(m, n)
+    var c = a - b
+    while (a != b) {
+        a = maxOf(c, b)
+        b = minOf(c, b)
+        c = a - b
+    }               //Алгоритм Евклида на нахождение НОД
+    return m * n / a //Связь НОК и НОД
+}
 
 /**
  * Средняя (3 балла)
@@ -129,7 +167,8 @@ fun lcm(m: Int, n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean =
+    lcm(m, n) / (m * n) == 1 //Связь НОК и НОД
 
 /**
  * Средняя (3 балла)
@@ -138,7 +177,15 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun revert(n: Int): Int = TODO()
+fun revert(n: Int): Int {
+    var res = 0
+    var number = n
+    while (number != 0) {
+        res = res * 10 + number % 10
+        number /= 10
+    }
+    return res
+}
 
 /**
  * Средняя (3 балла)
@@ -149,7 +196,8 @@ fun revert(n: Int): Int = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean =
+    revert(n) == n
 
 /**
  * Средняя (3 балла)
@@ -159,7 +207,16 @@ fun isPalindrome(n: Int): Boolean = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun hasDifferentDigits(n: Int): Boolean = TODO()
+fun hasDifferentDigits(n: Int): Boolean {
+    var number = n
+    var digit = number % 10
+    while (number > 0) {
+        if (digit != number % 10) return true
+        digit = number % 10
+        number /= 10
+    }
+    return false
+}
 
 /**
  * Средняя (4 балла)
