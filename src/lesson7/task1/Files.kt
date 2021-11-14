00e92d2db55e4de46e1eaab1a2f75402ea596015
Alexander Ganin <al_ganin@mail.ru>
2021-11-14 18:06:54 +0300

    Урок 6

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -63,7 +63,14 @@ fun alignFile(inputName: String, lineLength: Int, outputName: String) {
  * Подчёркивание в середине и/или в конце строк значения не имеет.
  */
 fun deleteMarked(inputName: String, outputName: String) {
-    TODO()
+    val writer = File(outputName).bufferedWriter()
+    for (line in File(inputName).readLines()) {
+        if (!line.startsWith('_')) {
+            writer.write(line)
+            writer.newLine()
+        }
+    }
+    writer.close()
 }
 
 /**
